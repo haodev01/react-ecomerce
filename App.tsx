6ee68ca1bf214b281f789f51feecd156b35ba096
haodev01 <haodev.01@gmail.com>
2024-12-20 14:30:28 +0700

    Fix

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {AppStackNavigator} from '~/routes/AppStackNavigator.tsx';
 import {Provider} from 'react-redux';
-import {store} from '~/store';
+import {store} from './src/store';
 function App(): React.JSX.Element {
   return (
     <Provider store={store}>
